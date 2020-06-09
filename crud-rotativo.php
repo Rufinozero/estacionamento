8635f29caa9a5d20da1ed91017a3c332756e7b9c
Victor Rufino <rufinozero@gmail.com>
2020-06-08 22:56:11 -0300

    update no crud-rotativo

--- crud-rotativo.php
+++ crud-rotativo.php
@@ -2,7 +2,7 @@
 
     function inicia_rotativo($conexao,$idvaga,$idcliente,$placa,$dataentrada){
         $query = "INSERT INTO FROTATIVO (VAGA,IDCLIENTE,PLACA,DTHR_ENTRADA)
-                        VALUES ('{$idvaga}','{$idcliente}','{$placa}','{$dataentrada}')";
+                        VALUES ('{$idvaga}','{$idcliente}','{$placa}',NOW())";
         return mysqli_query($conexao,$query);
     }
 
@@ -12,6 +12,13 @@
                     WHERE PLACA == PLACA
                     OR    IDCLIENTE == IDCLIENTE
                     OR    IDVAGA == IDVAGA";
+
+        return mysqli_query($conexao,$query);
+    }
+
+    function saida_rotativo($conexao,$idrotativo){
+        $query = "UPDATE FROTATIVO SET DTHR_SAIDA = NOW()
+                        WHERE IDROTATIVO == IDROTATIVO";
 
         return mysqli_query($conexao,$query);
     }
